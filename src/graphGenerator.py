9e9ad5b9b31ae3874813e5abcaec35a30a53311a
nathannn97 <36273759+nathannn97@users.noreply.github.com>
2019-11-14 16:40:57 +0000

    Update graphGenerator.py

--- src/graphGenerator.py
+++ src/graphGenerator.py
@@ -1,46 +1,63 @@
+from graphGenerator import ratingGraph
 import networkx as nx
+from abc import ABC
 import random
 
-# provides view of rating graph for briber
-class ratingGraph:
+#abstract briber class
+class briber(ABC):
+    def __init__(self, g, u0):
+        self.u = u0 # resources of briber to spend
+        self.g = g # network for agent
+        self.maxRating = self.g.maxRating
 
-    def __init__(self):
-        # Generate random ratings network
-        self.__g = nx.watts_strogatz_graph(30, 5, 0.3)
-        self.maxRating = 1
-        for n in self.__g.nodes:
-            rating = random.uniform(-0.25, self.maxRating)
-            if(rating < 0):
-                self.__g.nodes[n]["rating"] = None
-            else:
-                self.__g.nodes[n]["rating"] = rating
+    def bribe(self, id, amount):
+        if amount <= self.u:
+            self.g.bribe(id, amount)
+            self.u -= amount
 
-    #mean of neighbouring nodes for id
-    def pRating(self, id):
-        nds = [n for n in self.__g.neighbors(id) if self.getRating(n)]
-        if len(nds) == 0:
-            return 0
-        return sum(self.getRating(n) for n in nds) / len(nds)
+    def nextBribe(self):
+        pass
 
-    #mean of rating for all nodes
-    def oRating(self):
-        nds = [n for n in self.__g.nodes if self.getRating(n)]
-        return sum(self.getRating(n) for n in nds) / len(nds)
+# randomly assigns utility to bribes
+class randomBriber(briber):
+    def nextBribe(self):
+        initP = self.g.evalGraph()
+        customers = self.g.getCustomers()
+        # array of random bribes
+        brbs = [random.uniform(0.0, 1.0) for i in customers]
+        brbs = [b * self.u / sum(brbs) for b in brbs]
+        #enact bribes
+        for i in customers:
+            self.bribe(i, brbs[i])
+        #reassign utility based on new ratings
+        newP = self.g.evalGraph()
+        self.u += newP - initP
 
-    #returns customer ids without knowledge of edges or ratings
-    def getCustomers(self):
-        return list(self.__g.nodes)
+# Can see P-rating, can't see graph
+class influentialNodeBriber(briber):
+    def nextBribe(self):
+        k = 0.2
+        for c in self.g.getCustomers():
+            prevP = self.g.evalGraph()
+            #if voted and less that cost of info
+            if self.g.getRating(c) and self.g.getRating(c) < 1 - k:
+                self.bribe(c, k) # bribe for information
+                reward = self.g.evalGraph() - prevP - k
+                if reward > 0:
+                    #max out customers rating
+                    self.bribe(c, self.maxRating - self.g.getRating(c))
 
-    def getRating(self, id):
-        return self.__g.nodes[id]["rating"]
+            self.u += self.g.evalGraph() - prevP
 
-    # increase rating by bribe (up to max 5.0)
-    def bribe(self, id, b):
-        if self.getRating(id):
-            self.__g.nodes[id]["rating"] = min(self.maxRating, self.getRating(id) + b)
-        else:
-            self.__g.nodes[id]["rating"] = min(self.maxRating, b)
+def main():
+    g = ratingGraph()
+    inb = influentialNodeBriber(g, 100.0)
+    print(inb.u)
+    for i in range(20):
+        inb.nextBribe()
+        print(inb.u)
 
-    # evaluates reward of graph by suming P-ratings
-    def evalGraph(self):
-        return sum(self.pRating(n) for n in self.__g.nodes)
+
+
+if __name__ == '__main__':
+    main()
