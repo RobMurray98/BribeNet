029da83235759e11da709d5c8db5d37e644d6f1f
Finnbar Keating <finnjkeating@gmail.com>
2019-11-16 22:53:02 +0000

    Completed move to networkit.

--- src/graphGenerator.py
+++ src/graphGenerator.py
@@ -1,19 +1,21 @@
-import networkx as nx
+import networkit as nk
 import random
+import numpy as np
+
+ws_gen = nk.generators.WattsStrogatzGenerator(30, 5, 0.3)
 
 # provides view of rating graph for briber
 class ratingGraph:
 
-    def __init__(self):
+    def __init__(self, generator=ws_gen):
         # Generate random ratings network
-        self.__g = nx.watts_strogatz_graph(30, 5, 0.3)
+        self.__g = generator()
+        self.ratings = np.repeat(None, len(self.__g.nodes()))
         self.maxRating = 1
-        for n in self.__g.nodes:
+        for n in self.__g.nodes():
             rating = random.uniform(-0.25, self.maxRating)
-            if(rating < 0):
-                self.__g.nodes[n]["rating"] = None
-            else:
-                self.__g.nodes[n]["rating"] = rating
+            if(rating >= 0):
+                self.ratings[n] = rating
 
     #mean of neighbouring nodes for id
     def pRating(self, id):
@@ -24,23 +26,23 @@ class ratingGraph:
 
     #mean of rating for all nodes
     def oRating(self):
-        nds = [n for n in self.__g.nodes if self.getRating(n)]
+        nds = [n for n in self.__g.nodes() if self.getRating(n)]
         return sum(self.getRating(n) for n in nds) / len(nds)
 
     #returns customer ids without knowledge of edges or ratings
     def getCustomers(self):
-        return list(self.__g.nodes)
+        return list(self.__g.nodes())
 
     def getRating(self, id):
-        return self.__g.nodes[id]["rating"]
+        return self.ratings[id]
 
     # increase rating by bribe (up to max 5.0)
     def bribe(self, id, b):
         if self.getRating(id):
-            self.__g.nodes[id]["rating"] = min(self.maxRating, self.getRating(id) + b)
+            self.ratings[id] = min(self.maxRating, self.getRating(id) + b)
         else:
-            self.__g.nodes[id]["rating"] = min(self.maxRating, b)
+            self.ratings[id] = min(self.maxRating, b)
 
     # evaluates reward of graph by suming P-ratings
     def evalGraph(self):
-        return sum(self.pRating(n) for n in self.__g.nodes)
+        return sum(self.pRating(n) for n in self.__g.nodes())
--- src/snapImport.py
+++ src/snapImport.py
@@ -1,22 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 11 21:04:19 2019
 
-@author: callum
-"""
-
-import networkx as nx
-import matplotlib as plt
-import random
-    
-# Basic file open method
-# Tested only facebook, but format should be consistent for SNAP datasets
-f = open("data/facebook/facebook_combined.txt","rt")
-
-# Split each line on space char and remove trailing newline
-edges = []
-for line in f:
-    edges.append(line[:len(line)-1].split(" "))
-
-facebook = nx.Graph(edges)
+from networkit.graphio import SNAPGraphReader
+facebook = SNAPGraphReader().read("data/facebook/facebook_combined.txt")
