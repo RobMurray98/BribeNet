9aec3354b294a76f979b85a3088571e83885ef93
Robert Murray <rob98murray@gmail.com>
2019-11-19 23:01:52 +0000

    fix tkinter use

--- src/gui.py
+++ src/gui.py
@@ -15,16 +15,17 @@ import tkinter as tk
 
 rating_graph = RatingGraph()
 
+
 def buttonFunc(gtype, btype):
-    #print(gtype)
-    #print(btype)
-    if gtype=="ba":
+    # print(gtype)
+    # print(btype)
+    if gtype == "ba":
         print("not officially supported yet")
         return
     briber_setup = switch_briber(btype)
     print("Testing selected bribery method on chosen graph type!")
     graph_and_test(briber_setup, rating_graph.copy())
-    
+
 
 def switch_briber(argument):
     switcher = {
@@ -34,7 +35,7 @@ def switch_briber(argument):
     }
     return switcher.get(argument)
 
-    
+
 def graph_and_test(briber_setup, graph):
     print(graph.eval_graph())
     print("Bribing!")
@@ -42,6 +43,7 @@ def graph_and_test(briber_setup, graph):
     briber.next_bribe()
     print(graph.eval_graph())
     print("")
+
 
 root = tk.Tk()
 
@@ -51,23 +53,23 @@ v.set("L")
 v2 = tk.StringVar()
 v2.set("L")
 
-rb1 = Radiobutton(root, variable=v, value="ws", text="Watts-Strogatz")
-rb2 = Radiobutton(root, variable=v, value="ba", text="Barabási–Albert")
-rb1.grid(row=0,column=0)
-rb2.grid(row=1,column=0)
+rb1 = tk.Radiobutton(root, variable=v, value="ws", text="Watts-Strogatz")
+rb2 = tk.Radiobutton(root, variable=v, value="ba", text="Barabási–Albert")
+rb1.grid(row=0, column=0)
+rb2.grid(row=1, column=0)
 
-rba = Radiobutton(root, variable=v2, value="r", text="Random")
-rbb = Radiobutton(root, variable=v2, value="i", text="Influential")
-rbc = Radiobutton(root, variable=v2, value="mi", text="MostInfluential")
-rba.grid(row=0,column=1)
-rbb.grid(row=1,column=1)
-rbc.grid(row=2,column=1)
+rba = tk.Radiobutton(root, variable=v2, value="r", text="Random")
+rbb = tk.Radiobutton(root, variable=v2, value="i", text="Influential")
+rbc = tk.Radiobutton(root, variable=v2, value="mi", text="MostInfluential")
+rba.grid(row=0, column=1)
+rbb.grid(row=1, column=1)
+rbc.grid(row=2, column=1)
 
-b = Button(root, text="Graph + Test", command=lambda: buttonFunc(v.get(), v2.get()))
-b.grid(row=1,column=2)
+b = tk.Button(root, text="Graph + Test", command=lambda: buttonFunc(v.get(), v2.get()))
+b.grid(row=1, column=2)
 
 subwind = tk.Toplevel()
-b2 = Button(subwind, text="I am a button", command=buttonFunc)
-b2.pack(side=LEFT)
+b2 = tk.Button(subwind, text="I am a button", command=buttonFunc)
+b2.pack(side=tk.LEFT)
 
 root.mainloop()
