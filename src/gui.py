ac8b413ef3ea528cc63cb5d9c89495a08de5db58
Finnbar Keating <finnjkeating@gmail.com>
2019-11-23 21:11:35 +0000

    Apparently I never committed this so here I go

--- src/gui.py
+++ src/gui.py
@@ -12,11 +12,16 @@ from bribery.random import RandomBriber
 from graphGenerator import RatingGraph
 from parameterPrediction import test_parameter_prediction
 import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+from networkit.viztasks import drawGraph
 
 rating_graph = RatingGraph()
 
 
-def buttonFunc(gtype, btype):
+def generate_graph(gtype, btype):
     # print(gtype)
     # print(btype)
     if gtype == "ba":
@@ -24,7 +29,8 @@ def buttonFunc(gtype, btype):
         return
     briber_setup = switch_briber(btype)
     print("Testing selected bribery method on chosen graph type!")
-    graph_and_test(briber_setup, rating_graph.copy())
+    graph = graph_and_test(briber_setup, rating_graph.copy())
+    show_graph(graph)
 
 
 def switch_briber(argument):
@@ -43,6 +49,7 @@ def graph_and_test(briber_setup, graph):
     briber.next_bribe()
     print(graph.eval_graph())
     print("")
+    return graph
 
 
 root = tk.Tk()
@@ -65,11 +72,16 @@ rba.grid(row=0, column=1)
 rbb.grid(row=1, column=1)
 rbc.grid(row=2, column=1)
 
-b = tk.Button(root, text="Graph + Test", command=lambda: buttonFunc(v.get(), v2.get()))
+b = tk.Button(root, text="Graph + Test", command=lambda: generate_graph(v.get(), v2.get()))
 b.grid(row=1, column=2)
 
-subwind = tk.Toplevel()
-b2 = tk.Button(subwind, text="I am a button", command=buttonFunc)
-b2.pack(side=tk.LEFT)
+def show_graph(graph):
+    # subwind = tk.Toplevel()
+    # fig = Figure(figsize=(5,5))
+    drawGraph(graph.graph())
+    plt.show()
+    # canvas = FigureCanvasTkAgg(fig, subwind)
+    # canvas.draw()
+    # canvas.get_tk_widget().pack(fill=tk.BOTH, expand=1)
 
 root.mainloop()
