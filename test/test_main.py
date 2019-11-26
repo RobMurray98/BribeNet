74a6acd00577510cea386be82dca7b6778849cb1
RobMurray98 <rob98murray@gmail.com>
2019-11-26 15:52:19 +0000

    tests complete

--- test/test_main.py
+++ test/test_main.py
@@ -1,11 +1,36 @@
 import unittest
 
+from bribery.influentialNode import InfluentialNodeBriber
+from bribery.mostInfluencialNode import MostInfluentialNodeBriber
+from bribery.random import RandomBriber
+from graphGenerator import RatingGraph
+from parameterPrediction import test_parameter_prediction
+
 
 class TestMain(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.rating_graph = RatingGraph()
+
+    def tearDown(self) -> None:
+        del self.rating_graph
+
+    @staticmethod
+    def graph_and_test(briber_setup, graph):
+        print(graph.eval_graph())
+        print("Bribing!")
+        briber = briber_setup(graph)
+        briber.next_bribe()
+        print(graph.eval_graph())
+        print("")
+
     def test_main(self):
-        # unimplemented test
-        self.fail()
-
-
-if __name__ == '__main__':
-    unittest.main()
+        print("Testing parameter prediction!")
+        test_parameter_prediction()
+        print("")
+        print("Testing random bribery on a graph!")
+        self.graph_and_test(lambda g: RandomBriber(g, 10), self.rating_graph.copy())
+        print("Testing influential node bribery on a graph!")
+        self.graph_and_test(lambda g: InfluentialNodeBriber(g, 10, 0.2), self.rating_graph.copy())
+        print("Testing most influential node bribery on a graph!")
+        self.graph_and_test(lambda g: MostInfluentialNodeBriber(g, 10, 0.2), self.rating_graph.copy())
--- test/test_ratingGraph.py
+++ test/test_ratingGraph.py
@@ -1,43 +1,44 @@
 from unittest import TestCase
 
+from graphGenerator import RatingGraph
+
 
 class TestRatingGraph(TestCase):
+
+    def setUp(self) -> None:
+        self.rating_graph = RatingGraph()
+        self.__g = self.rating_graph.graph()
+
+    def tearDown(self) -> None:
+        del self.rating_graph
+
+    def test_neighbors(self):
+        for i in self.__g.nodes():
+            ns = self.rating_graph.neighbors(i)
+            self.assertTrue(len(ns) > 0)
+
     def test_p_rating(self):
-        # unimplemented test
-        self.fail()
+        for i in self.__g.nodes():
+            p = self.rating_graph.p_rating(i)
+            self.assertTrue(p >= 0)
 
     def test_median_p_rating(self):
-        # unimplemented test
-        self.fail()
+        for i in self.__g.nodes():
+            p = self.rating_graph.median_p_rating(i)
+            self.assertTrue(p >= 0)
 
     def test_sample_p_rating(self):
-        # unimplemented test
-        self.fail()
+        for i in self.__g.nodes():
+            p = self.rating_graph.sample_p_rating(i)
+            self.assertTrue(p >= 0)
 
     def test_o_rating(self):
-        # unimplemented test
-        self.fail()
-
-    def test_get_customers(self):
-        # unimplemented test
-        self.fail()
-
-    def test_customer_count(self):
-        # unimplemented test
-        self.fail()
-
-    def test_get_rating(self):
-        # unimplemented test
-        self.fail()
+        self.assertTrue(self.rating_graph.o_rating())
 
     def test_bribe(self):
-        # unimplemented test
-        self.fail()
-
-    def test_eval_graph(self):
-        # unimplemented test
-        self.fail()
-
-    def test_copy(self):
-        # unimplemented test
-        self.fail()
+        initial_value = self.rating_graph.eval_graph()
+        for i in self.__g.nodes():
+            g_copy = self.rating_graph.copy()
+            g_copy.bribe(i, 0.1)
+            bribed_value = g_copy.eval_graph()
+            self.assertTrue(initial_value != bribed_value)
