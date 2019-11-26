dcf51f528017cff94f9a202e0d4366d458ee2a6a
RobMurray98 <rob98murray@gmail.com>
2019-11-26 13:55:26 +0000

    add first test

--- test/bribery/test_briber.py
+++ test/bribery/test_briber.py
@@ -1,7 +1,23 @@
 from unittest import TestCase
 
+from bribery.briber import Briber
+from graphGenerator import RatingGraph
+
+
+class DummyBriber(Briber):
+    def __init__(self, g, u0):
+        super().__init__(g, u0)
+
 
 class TestBriber(TestCase):
+
+    def setUp(self) -> None:
+        self.briber = DummyBriber(RatingGraph(), 10)
+
+    def tearDown(self) -> None:
+        del self.briber
+
     def test_bribe(self):
         # unimplemented test
-        self.fail()
+        self.briber.bribe(0, 1)
+        self.assertEqual(self.briber.u, 9)
