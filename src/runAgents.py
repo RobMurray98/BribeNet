6cbca3301070992c82e36748ac1402061b113204
RobMurray98 <rob98murray@gmail.com>
2019-11-26 12:53:12 +0000

    PEP-8ify, add required dependencies to requirements.txt

--- src/runAgents.py
+++ src/runAgents.py
@@ -3,6 +3,7 @@ from bribery.oneMoveRandom import OneMoveRandom
 from bribery.oneMoveINB import OneMoveINB
 from graphGenerator import RatingGraph
 
+
 # Returns list of scores over the time of run
 # Inputs A and B should be the class of agent used
 def run_agents(A, B, init_u=10, moves=20):
@@ -10,40 +11,43 @@ def run_agents(A, B, init_u=10, moves=20):
     g1 = RatingGraph()
     g2 = g1.copy()
     # Agents running on identical graphs
-    agentA = A(g1, init_u)
-    agentB = B(g2, init_u)
+    agent_a = A(g1, init_u)
+    agent_b = B(g2, init_u)
     # scores over time
-    scoresA = [g1.eval_graph()]
-    scoresB = [g2.eval_graph()]
+    scores_a = [g1.eval_graph()]
+    scores_b = [g2.eval_graph()]
 
     for i in range(0, moves):
         # Bribe A
-        agentA.next_bribe()
-        scoresA.append(g1.eval_graph())
+        agent_a.next_bribe()
+        scores_a.append(g1.eval_graph())
         # Bribe B
-        agentB.next_bribe()
-        scoresB.append(g2.eval_graph())
+        agent_b.next_bribe()
+        scores_b.append(g2.eval_graph())
 
-    return scoresA, scoresB
+    return scores_a, scores_b
+
 
 # plots two lists of scores on a graph, saved to filename
-def plot_scores(scoresA, scoresB, labelA, labelB, filename="graphrun.png"):
-    print(scoresA)
-    print(scoresB)
-    print(labelA)
-    print(labelB)
-    xs = [i for i in range(0, len(scoresA))]
-    plt.plot(xs, scoresA, color="red", label=labelA)
-    plt.plot(xs, scoresB, color="orange", label=labelB)
+def plot_scores(scores_a, scores_b, label_a, label_b, filename="graphrun.png"):
+    print(scores_a)
+    print(scores_b)
+    print(label_a)
+    print(label_b)
+    xs = [i for i in range(0, len(scores_a))]
+    plt.plot(xs, scores_a, color="red", label=label_a)
+    plt.plot(xs, scores_b, color="orange", label=label_b)
     plt.xlabel("Moves over time")
     plt.ylabel("Average P-rating")
     plt.legend(loc="upper left")
     plt.savefig(filename)
     plt.clf()
 
+
 def main():
-    A, B = run_agents(OneMoveRandom, OneMoveINB)
-    plot_scores(A, B, "Random Bribing Agent", "Influential Node Bribing Agent")
+    a, b = run_agents(OneMoveRandom, OneMoveINB)
+    plot_scores(a, b, "Random Bribing Agent", "Influential Node Bribing Agent")
+
 
 if __name__ == '__main__':
     main()
