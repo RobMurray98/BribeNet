04456c2b388b7b4cdcfd1d8ff36a70ee6cac8d97
RobMurray98 <rob98murray@gmail.com>
2020-01-17 07:05:57 +0000

    #29 add relevant type assertions to StaticRatingGraph

--- src/graph/static/staticRatingGraph.py
+++ src/graph/static/staticRatingGraph.py
@@ -1,7 +1,31 @@
 from abc import ABC
 
-from graph.ratingGraph import RatingGraph
+from graph.ratingGraph import RatingGraph, DEFAULT_GEN
+
+import numpy as np
 
 
 class StaticRatingGraph(RatingGraph, ABC):
-    pass
+
+    def __init__(self, generator=DEFAULT_GEN, specifics=None, **kwargs):
+        super().__init__(generator=generator, specifics=specifics, **kwargs)
+
+    def __finalise_init(self):
+        """
+        Perform assertions that ensure everything is initialised
+        """
+        assert self.get_bribers() is not None, "specifics of implementing class did not instantiate self._bribers"
+        from bribery.static.staticBriber import StaticBriber
+        if issubclass(self.get_bribers().__class__, StaticBriber):
+            # noinspection PyProtectedMember
+            self._bribers._set_graph(self)
+        else:
+            assert isinstance(self.get_bribers(), tuple), "bribers on graph not instantiated as a tuple and not a " \
+                                                          "single briber"
+            for briber in self.get_bribers():
+                assert issubclass(briber.__class__, StaticBriber), "member of graph bribers not an instance of a " \
+                                                                   "subclass of StaticBriber"
+                # noinspection PyProtectedMember
+                briber._set_graph(self)
+        assert type(self._votes) is np.ndarray, "specifics of implementing class did not instantiate self._votes to " \
+                                                "an ndarray"
