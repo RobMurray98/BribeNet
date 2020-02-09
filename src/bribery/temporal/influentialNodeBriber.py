2135f132229afdef95ce7af3b460ad0eec03476a
u1606521 <C.Marvell@warwick.ac.uk>
2020-02-09 17:29:26 +0000

    influential briber (temporal) now passes all tests

--- src/bribery/temporal/influentialNodeBriber.py
+++ src/bribery/temporal/influentialNodeBriber.py
@@ -1,6 +1,6 @@
 from bribery.temporal.briber import TemporalBriber
 from bribery.temporal.action.singleBriberyAction import SingleBriberyAction
-from random import random
+import random
 
 
 class InfluentialNodeBriber(TemporalBriber):
@@ -15,23 +15,22 @@ class InfluentialNodeBriber(TemporalBriber):
         self.npr = 0
         self.nprd = 0
 
-    @override
     def _set_graph(self, g):
         super()._set_graph(g)
         # Make sure that k is set such that there are enough resources left to actually bribe people.
         self.k = min(self.k, 0.5 * (self._u / self._g.customer_count()))
-        self.pr = self.get_graph.eval_graph(self.get_briber_id)
+        self.pr = self.get_graph().eval_graph(self.get_briber_id())
         self.npr = self.pr
 
     def next_action(self) -> SingleBriberyAction:
         nextAct = SingleBriberyAction(self)
         if self.npr>self.pr:
-            nextAct.add_bribe(self.nprd, min(self.get_resources, (1-self.get_graph.getvote(self.nprd))))
+            nextAct.add_bribe(self.nprd, min(self.get_resources(), (1-self.get_graph().getvote(self.nprd))))
             self.pr = self.npr
         else:
-            mNode = len(self.get_graph.nodes)
-            nNode = random.randInt(mNode)
+            mNode = len(self.get_graph().get_votes())
+            nNode = random.randint(0,mNode-1)
             nextAct.add_bribe(nNode, self.k)
-            self.npr = self.get_graph.eval_graph(self.get_briber_id)
+            self.npr = self.get_graph().eval_graph(self.get_briber_id())
             self.nprd = nNode
         return nextAct
