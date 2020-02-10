8f95f94a9e8c1885b437945131abb0814ffe2a84
Callum <C.Marvell@warwick.ac.uk>
2020-02-10 22:04:22 +0000

    added basic tests for new bribers

---
+++ test/bribery/temporal/test_budgetBriber.py
@@ -0,0 +1,21 @@
+from copy import deepcopy
+
+from bribery.temporal.budgetNodeBriber import BudgetNodeBriber
+from graph.temporal.ratingGraph import TemporalRatingGraph
+from test.bribery.temporal.briberTestCase import BriberTestCase
+
+
+class TestRandomBriber(BriberTestCase):
+
+    def setUp(self) -> None:
+        self.briber = BudgetNodeBriber(10)
+        self.rg = TemporalRatingGraph(self.briber)
+
+    def test_next_action_increases_p_rating(self):
+        graph = self.briber._g
+        action = self.briber.next_action()
+        briber_id = self.briber.get_briber_id()
+        prev_eval = graph.eval_graph(briber_id=briber_id)
+
+        action.perform_action()
+        self.assertGreater(graph.eval_graph(briber_id=briber_id), prev_eval)
