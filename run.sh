551ea7d530d4b595c0ece46eaa3e2a816bb3c205
Robert Murray <rob98murray@gmail.com>
2020-05-01 10:47:58 +0100

    remove line from run.sh

--- run.sh
+++ run.sh
@@ -5,9 +5,8 @@ then
   export PYTHONPATH=$PYTHONPATH:$SCRIPTPATH/src
 fi
 apt-get install -y docker.io x11-xserver-utils  # ensure docker and xhost commands are available
-systemctl start docker
-service docker enable
-service docker start
+systemctl start docker  # ensure docker daemon is running
+service docker start  # ensure docker service is running
 docker build -t temporal_model_gui .  # build the docker container
 xhost +  # let the docker container use the host display
 docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix temporal_model_gui  # run the docker container
